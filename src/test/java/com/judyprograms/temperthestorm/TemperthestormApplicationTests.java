3961880258fd651b72b08d00d345a175601918e3
JudyPrograms <judithkats@gmail.com>
2021-11-09 18:43:00 +0100

    initial commit

---
+++ src/test/java/com/judyprograms/temperthestorm/TemperthestormApplicationTests.java
@@ -0,0 +1,13 @@
+package com.judyprograms.temperthestorm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemperthestormApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
